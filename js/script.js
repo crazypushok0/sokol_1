12705ba0bfddf296f1564f7ec2fd81536b99e9f3
crazypushok0 <crazypushok0@gmail.com>
2020-07-14 08:35:37 +0300

    about page

--- js/script.js
+++ js/script.js
@@ -48,8 +48,51 @@ $(function () {
 		}
 
 	});
-	// $('.trust__item').click(function () {
-	// 	$('.trust__item.active').removeClass('active');
-	// 	$(this).addClass('active');
-	// });
+	$(".about-gallery__items").slick({
+		slidesToShow: 1,
+		dots: true,
+		arrows: true,
+		prevArrow: '<div class="slick-prev">&#10094;</div>',
+		nextArrow: '<div class="slick-next">&#10095;</div>',
+		customPaging: function (slider, i) {
+			let current = i + 1;
+			current = current < 10 ? "0" + current : current;
+
+			let total = slider.slideCount;
+			total = total < 10 ? "0" + total : total;
+
+			return (
+				'<button type="button" role="button" tabindex="0" class="slick-dots-button">\
+			<span class="slick-dots-current">' + current + '</span>\
+			<span class="slick-dots-separator"></span>\
+			<span class="slick-dots-total">' + total + '</span>\
+		</button>'
+			);
+		}
+
+	});
+	$(".about-articles__slider").slick({
+		slidesToShow: 4,
+		dots: true,
+		arrows: true,
+		prevArrow: '<div class="slick-prev">&#10094;</div>',
+		nextArrow: '<div class="slick-next">&#10095;</div>',
+		customPaging: function (slider, i) {
+			let current = i + 1;
+			current = current < 10 ? "0" + current : current;
+
+			let total = slider.slideCount;
+			total = total < 10 ? "0" + total : total;
+
+			return (
+				'<button type="button" role="button" tabindex="0" class="slick-dots-button">\
+			<span class="slick-dots-current">' + current + '</span>\
+			<span class="slick-dots-separator"></span>\
+			<span class="slick-dots-total">' + total + '</span>\
+		</button>'
+			);
+		}
+
+	});
+
 })
